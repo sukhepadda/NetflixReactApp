605900a49ee77e9690f6fde595bbc5d780716df0
sukhepadda <paddasukhwantsingh@gmail.com>
2023-02-28 22:55:39 +0530

    works continuing

---
+++ src/Row.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import axios from "./axios"; //its renamed of instence because we used default we can change the name but if we doesnt used default we have to use specific word
+
+import "./Row.css";
+
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+function Row({ title, fetchUrl, isLargeRow }) {
+  const [movies, setMovies] = useState([]);
+  // a snippet for code which run based on specific condition
+  useEffect(() => {
+    // if [], run once when the row loads, and dont run again
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+
+      <div className="row_posters">
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+            src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Row;
