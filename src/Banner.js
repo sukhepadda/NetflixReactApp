b61e0de0f646a78ce7de07e40b8ec8f35701aafb
sukhepadda <paddasukhwantsingh@gmail.com>
2023-03-01 14:34:27 +0530

    completed upto nav banner and scroll rows

--- src/Banner.js
+++ src/Banner.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import axios from "./axios";
 import requests from "./requests";
 
+import "./Banner.css";
+
 
 function Banner() {
     const [movie, setMovies] = useState([]);
@@ -19,8 +21,9 @@ function Banner() {
       fetchData();
     }, []);
     
-    console.log(movie)
-
+    function truncate(str, n){
+      return str?.length > n ? str.substr(0, n-1) + "..." : str;
+    }
   return <header className="banner"
   style={{
     backgroundSize: "cover",
@@ -29,15 +32,16 @@ function Banner() {
   }}
   >
     <div className="banner_contents">
-        <h1>
+        <h1 className="banner_title">
             {movie?.title || movie?.name || movie?.original_name}
         </h1>
         <div className="banner_buttons">
             <button className="banner_button">Play</button>
             <button className="banner_button">My List</button>
         </div>
-        <h1 className="banner_description">{movie?.overview}</h1>
+        <h1 className="banner_description">{truncate(movie?.overview, 150)}</h1>
     </div>
+    <div className="banner_fadeBottom"></div>
   </header>;
 }
 
