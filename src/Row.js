747764913244d2d2e081040274116f77911bd09a
sukhepadda <paddasukhwantsingh@gmail.com>
2023-03-01 19:24:57 +0530

    done as per tutorial only hosting remains

--- src/Row.js
+++ src/Row.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios"; //its renamed of instence because we used default we can change the name but if we doesnt used default we have to use specific word
-
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 import "./Row.css";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState([""]);
   // a snippet for code which run based on specific condition
   useEffect(() => {
     // if [], run once when the row loads, and dont run again
@@ -18,6 +20,33 @@ function Row({ title, fetchUrl, isLargeRow }) {
     fetchData();
   }, [fetchUrl]);
 
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl){
+      setTrailerUrl("");
+    }else{
+      movieTrailer(movie?.title || movie?.name || movie?.original_name || "")
+      .then( (url) => {
+        const urlParams = new URLSearchParams( new URL(url).search);
+        setTrailerUrl(urlParams.get('v'));
+      })
+      .catch((error) => console.log(error));
+    }
+  }
+  const handleClick2 = () => {
+    if (trailerUrl){
+      setTrailerUrl("");
+    }
+  }
+
+
   return (
     <div className="row">
       <h2>{title}</h2>
@@ -27,11 +56,16 @@ function Row({ title, fetchUrl, isLargeRow }) {
           <img
             key={movie.id}
             className={`row_poster ${isLargeRow && "row_posterLarge"}`}
-            src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
             alt={movie.name}
+            onClick={() => handleClick(movie)}
+            onLoad={() =>  handleClick2()}
           />
         ))}
       </div>
+      { trailerUrl && <YouTube videoId={trailerUrl} opts={opts} /> }
     </div>
   );
 }
